843f895b11a6bc0d3267e5364711a817abd60dc5
Thibault Garcia <thibault.garcia.pro@gmail.com>
2024-10-10 09:23:02 +0200

    add pokemon class

---
+++ assets/scripts/Pokemon.js
@@ -0,0 +1,48 @@
+export default class Pokemon {
+  constructor(id, name, img, weight, height, types, abilities) {
+    this.id = id;
+    this.name = name;
+    this.img = img;
+    this.weight = weight;
+    this.height = height;
+    this.types = types;
+    this.abilities = abilities;
+  }
+
+  render(container) {
+    const pokemonPicture = container.querySelector("#pokemon-picture");
+    pokemonPicture.src = this.img;
+
+    const pokemonId = container.querySelector("#pokemon-id");
+    pokemonId.textContent = `#${this.id}`;
+
+    const pokemonName = container.querySelector("#pokemon-name");
+    pokemonName.innerHTML = this.name;
+
+    const pokemonWeight = container.querySelector("#pokemon-weight");
+    pokemonWeight.innerHTML = `${this.weight.toFixed(1)} kg`;
+
+    const pokemonHeight = container.querySelector("#pokemon-height");
+    pokemonHeight.innerHTML = `${this.height.toFixed(1)} m`;
+
+    const pokemonTypes = container.querySelector("#pokemon-types");
+    pokemonTypes.innerHTML = "";
+    this.types.forEach((type) => {
+      const pokemonTypeLi = document.createElement("li");
+      const pokemonTypeSpan = document.createElement("span");
+      pokemonTypeSpan.innerHTML = type;
+      pokemonTypeLi.appendChild(pokemonTypeSpan);
+      pokemonTypes.appendChild(pokemonTypeLi);
+    });
+
+    const pokemonAbilities = container.querySelector("#pokemon-abilities");
+    pokemonAbilities.innerHTML = "";
+    this.abilities.forEach((ability) => {
+      const pokemonAbilityLi = document.createElement("li");
+      const pokemonAbilitySpan = document.createElement("span");
+      pokemonAbilitySpan.innerHTML = ability;
+      pokemonAbilityLi.appendChild(pokemonAbilitySpan);
+      pokemonAbilities.appendChild(pokemonAbilityLi);
+    });
+  }
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,81 +1,78 @@
+import Pokemon from "./Pokemon.js";
+
 const API_URL = "https://pokeapi.co/api/v2";
 
 const pokemonSearch = document.querySelector("#pokemon-search");
 const searchInput = document.querySelector("#search-input");
 const pokemonDisplay = document.querySelector("#pokemon-display");
-const pokemonPicture = document.querySelector("#pokemon-picture");
-const pokemonId = document.querySelector("#pokemon-id");
-const pokemonName = document.querySelector("#pokemon-name");
-const pokemonWeight = document.querySelector("#pokemon-weight");
-const pokemonHeight = document.querySelector("#pokemon-height");
-const pokemonTypes = document.querySelector("#pokemon-types");
-const pokemonAbilities = document.querySelector("#pokemon-abilities");
 const pokemonPrevious = document.querySelector("#pokemon-previous");
 const pokemonNext = document.querySelector("#pokemon-next");
 
-pokemonDisplay.style.display = "none";
-pokemonPrevious.style.display = "none";
-pokemonNext.style.display = "none";
+const getPokemon = async (search) => {
+  try {
+    const data = await (await fetch(`${API_URL}/pokemon/${search}`)).json();
+    const pokemon = new Pokemon(
+      data.id,
+      `${data.name.substring(0, 1).toUpperCase()}${data.name
+        .substring(1)
+        .toLowerCase()}`,
+      data.sprites.front_default,
+      data.weight / 10,
+      data.height / 10,
+      data.types.map(
+        (type) =>
+          `${type.type.name.substring(0, 1).toUpperCase()}${type.type.name
+            .substring(1)
+            .toLowerCase()}`
+      ),
+      data.abilities.map(
+        (ability) =>
+          `${ability.ability.name
+            .substring(0, 1)
+            .toUpperCase()}${ability.ability.name.substring(1).toLowerCase()}`
+      )
+    );
 
-const getPokemon = async (pokemon) => {
-  const data = await (await fetch(`${API_URL}/pokemon/${pokemon}`)).json();
+    pokemon.render(pokemonDisplay);
 
-  pokemonDisplay.style.display = "block";
-  pokemonPrevious.style.display = "block";
-  pokemonNext.style.display = "block";
+    if (pokemon.id > 1) {
+      pokemonPrevious.removeAttribute("disabled");
+      pokemonPrevious.onclick = async () => {
+        await getPokemon(pokemon.id - 1);
+      };
+    } else {
+      pokemonPrevious.setAttribute("disabled", true);
+    }
 
-  pokemonPicture.src = data.sprites.front_default;
-  pokemonId.innerHTML = `#${data.id}`;
-  pokemonName.innerHTML = `${data.name.substring(0, 1).toUpperCase()}${data.name
-    .substring(1)
-    .toLowerCase()}`;
-  pokemonWeight.innerHTML = `${(data.weight / 10).toFixed(1)} kg`;
-  pokemonHeight.innerHTML = `${(data.height / 10).toFixed(1)} m`;
-
-  pokemonTypes.innerHTML = "";
-  data.types.forEach((type) => {
-    const pokemonTypeLi = document.createElement("li");
-    const pokemonTypeSpan = document.createElement("span");
-    pokemonTypeSpan.innerHTML = `${type.type.name
-      .substring(0, 1)
-      .toUpperCase()}${type.type.name.substring(1).toLowerCase()}`;
-    pokemonTypeLi.appendChild(pokemonTypeSpan);
-    pokemonTypes.appendChild(pokemonTypeLi);
-  });
-
-  pokemonAbilities.innerHTML = "";
-  data.abilities.forEach((ability) => {
-    const pokemonAbilityLi = document.createElement("li");
-    const pokemonAbilitySpan = document.createElement("span");
-    pokemonAbilitySpan.innerHTML = `${ability.ability.name
-      .substring(0, 1)
-      .toUpperCase()}${ability.ability.name.substring(1).toLowerCase()}`;
-    pokemonAbilityLi.appendChild(pokemonAbilitySpan);
-    pokemonAbilities.appendChild(pokemonAbilityLi);
-  });
-
-  if (data.id > 1) {
-    pokemonPrevious.removeAttribute("disabled");
-    pokemonPrevious.onclick = async () => {
-      await getPokemon(data.id - 1);
-    };
-  } else {
-    pokemonPrevious.setAttribute("disabled", true);
-  }
-
-  if (data.id < 1025) {
-    pokemonNext.removeAttribute("disabled");
-    pokemonNext.onclick = async () => {
-      await getPokemon(data.id + 1);
-    };
-  } else {
-    pokemonNext.setAttribute("disabled", true);
+    if (pokemon.id < 1025) {
+      pokemonNext.removeAttribute("disabled");
+      pokemonNext.onclick = async () => {
+        await getPokemon(pokemon.id + 1);
+      };
+    } else {
+      pokemonNext.setAttribute("disabled", true);
+    }
+  } catch (error) {
+    pokemonDisplay.classList.remove("show");
+    pokemonDisplay.classList.add("hide");
+    pokemonPrevious.classList.remove("show");
+    pokemonPrevious.classList.add("hide");
+    pokemonNext.classList.remove("show");
+    pokemonNext.classList.add("hide");
   }
 };
 
 pokemonSearch.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const pokemon = searchInput.value.toLowerCase();
+  const search = searchInput.value.toLowerCase();
   searchInput.value = "";
-  await getPokemon(pokemon);
+
+  pokemonDisplay.classList.remove("hide");
+  pokemonDisplay.classList.add("show");
+  pokemonPrevious.classList.remove("hide");
+  pokemonPrevious.classList.add("show");
+  pokemonNext.classList.remove("hide");
+  pokemonNext.classList.add("show");
+
+  await getPokemon(search);
 });
